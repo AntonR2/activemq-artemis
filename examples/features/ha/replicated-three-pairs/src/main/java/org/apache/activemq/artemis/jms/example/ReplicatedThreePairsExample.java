dcb8b218930a1fec37ab83b90955f96c4294b319
Justin Bertram <jbertram@apache.org>
2017-10-02 13:30:33 -0500

    ENTMQBR-783 example for testing 3 replicated HA pairs

---
+++ examples/features/ha/replicated-three-pairs/src/main/java/org/apache/activemq/artemis/jms/example/ReplicatedThreePairsExample.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.activemq.artemis.jms.example;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+
+import org.apache.activemq.artemis.util.ServerUtil;
+
+public class ReplicatedThreePairsExample {
+   public static void main(final String[] args) throws Exception {
+      System.out.println("\n\n\nPress any key to continue...\n\n\n");
+      System.in.read();
+   }
+}
